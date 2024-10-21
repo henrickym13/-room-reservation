c24429139c668036a5a3e1af6fcf3ebda199ff99
unknown <henrique.so.ad@hotmail.com>
2024-10-21 19:25:42 -0300

    new feature PDF generator

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('report/', views.usage_report, name='usage_report'),
+    path('pdf/', views.render_pdf_view, name='exportar_relatorio_completo'),
     path('', include('user.urls')),
     path('', include('room.urls')),
     path('', include('reserve.urls')),
